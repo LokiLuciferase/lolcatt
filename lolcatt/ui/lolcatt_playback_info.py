9b7965ef9b8349d6bdeed5fcbdfe9c3d3f178583
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-07-04 14:54:03 +0200

    add marquee for cast content

--- lolcatt/ui/lolcatt_playback_info.py
+++ lolcatt/ui/lolcatt_playback_info.py
@@ -1,13 +1,19 @@
 from textual.containers import Container
 from textual.widgets import Label
+from textual.reactive import reactive
 
 from lolcatt.ui.caster_static import CasterStatic
+from lolcatt.utils.utils import marquee
 
 
 class LolCattPlaybackInfo(CasterStatic):
+
+    label_str = reactive('')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label = Label('', id='title')
+        self._marquee_gen = None
 
     def _get_playback_info(self) -> str:
         playing = self._caster.get_cast_state().cast_info.get('title')
@@ -21,7 +27,14 @@ class LolCattPlaybackInfo(CasterStatic):
                 return f'Nothing is playing.'
 
     def _update_label(self):
-        self.label.update(self._get_playback_info())
+        self.label_str = self._get_playback_info() + ' '
+        self.label.update(next(self._marquee_gen))
+
+    def watch_label_str(self, value):
+        self._marquee_gen = marquee(value, self.size.width, 2)
+
+    def on_resize(self, value):
+        self._marquee_gen = marquee(self.label_str, self.size.width, 2)
 
     def compose(self):
         yield Container(self.label, id='playback_info')
