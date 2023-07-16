c35abe2aea30742317c51e25b992e5ecf996084f
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-07-16 12:13:56 +0200

    show catt aliases in `lolcatt --scan` output

--- lolcatt/utils/utils.py
+++ lolcatt/utils/utils.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 
 from catt.api import discover
+from catt.cli import get_config_as_dict
 
 
 def marquee(s: str, max_len: int, step_size: int) -> str:
@@ -25,24 +26,37 @@ def scan():
     """Scans the network for Chromecast devices and prints the results."""
     print('Scanning for Chromecast devices...')
 
+    # Invert aliases dict
+    aliases = get_config_as_dict().get('aliases', {})
+    devices_to_aliases = {v: k for k, v in aliases.items()}
+
     discovered = discover()
     deduped = []
     found_ips = set()
     max_name_len = 11  # len('Device name')
+    max_alias_len = 5  # len('Alias')
     for device in discovered:
         if device.ip_addr not in found_ips:
             found_ips.add(device.ip_addr)
             max_name_len = max(max_name_len, len(device.name))
-            deduped.append((device.name, device.ip_addr))
+            max_alias_len = max(max_alias_len, len(devices_to_aliases.get(device.name, '')))
+            deduped.append((device.name, devices_to_aliases.get(device.name, ''), device.ip_addr))
 
-    deduped = [('"' + x + '"' + ' ' * (max_name_len - len(x)), y) for x, y in deduped]
+    deduped = [
+        (
+            '"' + x + '"' + ' ' * (max_name_len - len(x)),
+            ('"' + y + '"' if len(y) else '') + ' ' * (max_alias_len - len(y)),
+            z,
+        )
+        for x, y, z in deduped
+    ]
     deduped = sorted(deduped, key=lambda x: x[1])
 
     print('Found {} device(s):'.format(len(found_ips)))
-    print(f'Device name{" " * (max_name_len - 11)}\tDevice IP')
-    print('=' * (max_name_len + 19))
-    for name, ip in deduped:
-        print(f'{name}\t{ip}')
+    print(f'Device name{" " * (max_name_len - 11)}\tAlias{" " * (max_alias_len - 5) }\tDevice IP')
+    print('=' * (max_name_len + max_alias_len + 22))
+    for name, alias, ip in deduped:
+        print(f'{name}\t{alias}\t{ip}')
 
 
 def write_initial_config(p: Path):
